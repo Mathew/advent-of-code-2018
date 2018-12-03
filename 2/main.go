c197ba2d97592fc06b0c8c54aa944f0e3f16ad0a
Mathew Taylor <mat@mathewtaylor.co.uk>
2018-12-03 21:54:50 +0000

    First 3 days.

---
+++ 2/main.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const rawBoxIds = `icxjubroqtunzeyzpomfksahgw
+ibxjvbroqtunleyzjdmfksahow
+icxjvbroqtinleyzpdmflsahnw
+icxjvbnoqtunleyvpgmfksahgw
+wcxjvbroqrunleyzpdmfksahge
+icxjtbroqtjzleyzpdmfksahgw
+icxjvbrohtunleyzpdmfkbahsw
+xcxjvbroqcunleyzpdmfksaxgw
+ycxjvbroqtunleyzpowfksahgw
+icxfvbroqtunleyzpdmfksncgw
+ixxjvbuoqtunleyzpdvfksahgw
+icfjvbroqtunleyzpdmfksadgt
+icxjvbroqdunleyzpdafksqhgw
+icxjvbrobtunlelzpdmfkuahgs
+ujxjvbroqtunleyzpdmqksahgw
+icqjvsroqtunleyzpdmfksahuw
+icxjvbroptpnleyzpdmfksangw
+ipxjvbroqtunleyzpdmfesahgi
+icajvbroqtunltyzpdqfksahgw
+ickjvbroqtuzleyzpdmfgsahgw
+icxjvbroqtunledzpdmwksahgz
+icxjvlroqtsnleyzpdmfksvhgw
+icxjvbroqtunleyzpdsfysahvw
+icxjvbroqtunwnyzydmfksahgw
+ionjvbroqtunleyzpdmfksahgj
+icxjvwriqtunleyzpdmfksahgi
+ocxjvbroztunleyzpdmfksapgw
+icxjvbroqtmnlewzpumfksahgw
+ucxjvbroqtunleyzpdmzktahgw
+icxgvbroqtunleyztdmfktahgw
+icxhvbroqttnleybpdmfksahgw
+icxjvbroqtugleyzpdxfkeahgw
+acxjvbroqvunlexzpdmfksahgw
+icxjvbroqglnleyzpbmfksahgw
+icxjvbriqtnvleyzpdmfksahgw
+icxjvbreqtunlakzpdmfksahgw
+gcxjvbuoqtunleyzpdmfksawgw
+icxjvbroqtunleyzpddfkyzhgw
+icxjvbjoqtunleyzpdmfqsahhw
+icxjvjroqtunleyzpnmfksajgw
+ycxjvbroqtunmeyzcdmfksahgw
+irxkvbryqtunleyzpdmfksahgw
+isxjvbrlqtunleyzpdmsksahgw
+icxjvbcoqtunleyzpdfkksahgw
+ixnjvbroqtunleyzpdmfkqahgw
+wcxjvbroqhunleyzqdmfksahgw
+icljvurmqtunleyzpdmfksahgw
+ibxjvbroqtayleyzpdmfksahgw
+arxjvbroqiunleyzpdmfksahgw
+iuxjvbroqtunluyzpdmoksahgw
+icxjvbrmqtunleyzpdmfosahew
+isxjvbroqtunleyrpdmfksrhgw
+icxjvxrpqtunleyzpdmfkdahgw
+ichjvbrogtunllyzpdmfksahgw
+icxjvbeoqtunlryzpdmfksakgw
+icxjvbroqtcnemyzpdmfksahgw
+icxjvbroqtybledzpdmfksahgw
+icxjvbrqqtunleyzpdmfksgngw
+icgjvbroqtunleyzmdmfksabgw
+icxjtbroqtunleyzkdmfksahww
+icxjvbfoqtunleyzpamfqsahgw
+icxjvbroknuyleyzpdmfksahgw
+icxjvbroqtujleyzpdmaksaigw
+icxjvbroqtnnlmyzpdmflsahgw
+icxjvbroqtunlefzpdmfsfahgw
+icxjvdroqtusleyzpdzfksahgw
+icxjvbioqtunlsyzpdmfkshhgw
+icxbvbrodtunleyzpdmoksahgw
+icxjvbroqtuvleyzpdmfkbahmw
+iyxjvbroqtunljvzpdmfksahgw
+icxjvbroqtudleynddmfksahgw
+icxjvwroqtnnleyzpdmfksahgz
+ichjvbroqtunleyzjdmeksahgw
+icxjvbrostunluyrpdmfksahgw
+icfjvbroqtunleyxpdgfksahgw
+nhxjvbroqtunlerzpdmfksahgw
+icxjvbrothunlexzpdmfksahgw
+icxjvbrzltqnleyzpdmfksahgw
+icxjvbrhqtunleyzpdmfksajgy
+vcxjvjroqiunleyzpdmfksahgw
+icxjfbroltunleyzpdmqksahgw
+icxbvbroqtunleyzpdofasahgw
+icxjvbkoqtunveyzpdmfksaqgw
+icxsebroqtunleyzpdmuksahgw
+icxjvbroquunlpyrpdmfksahgw
+icxhvbroqtunjeyzpdmrksahgw
+icdjvbroqtunlzyzpdmfksangw
+jcxqvbroqtvnleyzpdmfksahgw
+icxjvxroqtunleyrpdmfxsahgw
+icxjvnroqtunleyzpdmfssyhgw
+icxjvbraptunleyzpdofksahgw
+icxjvbroatunleyjpdmfbsahgw
+icxjvbroytlnlryzpdmfksahgw
+iaxjvbroqkunleyzpdmfcsahgw
+ucxjvbroqtuileyzzdmfksahgw
+icxjqbroqtcnleyzpgmfksahgw
+icxjvbloqtunleyzadmfksaqgw
+icxjvbroqtunleyzkdmnksakgw
+icxjvbroqtunleyjpdxfksahvw
+iqxjvbroqtujleyzpdmfklahgw
+icgjvbroqtunleyzpdmfksbhgb
+icxjzbroqtunleyzpdmfkdahgg
+icxjvbrobtunloywpdmfksahgw
+icxavbroqtunleyfpdmfksahgd
+icxjvbroqtunleyophmfksahkw
+icxjndroqtunlyyzpdmfksahgw
+icxjvbroqtjnleyppdmvksahgw
+icxjvbroonfnleyzpdmfksahgw
+icxjvbrqqtlnljyzpdmfksahgw
+icxjvbrzqtunlelspdmfksahgw
+icxjvbooqtunleyztdmfkfahgw
+icajvbroltunlnyzpdmfksahgw
+icxjvbroqtunleyzidmdkschgw
+icxjvbroktupleyzpdmfksahyw
+icxjcbroyeunleyzpdmfksahgw
+icxjvbroqtunlezkpdmfksahsw
+icxjvbroqtunlejzpcmfksrhgw
+icxjvvroqtunlsyzkdmfksahgw
+icxjnbroqtunbeyzpdmfpsahgw
+itxjbbroqtunleyzpemfksahgw
+icxjvbroqtunlrkzpdmfksshgw
+rcxjvbroqtujlbyzpdmfksahgw
+icxjvmroqtugleazpdmfksahgw
+icxjvbfoqtunleylpdmfkeahgw
+icnjvoroktunleyzpdmfksahgw
+icxjvbroqtunlvyzpdmfkfahgr
+icxjvbroqtgnseyzpdmfxsahgw
+scxjvbroqtunleycpdmfksdhgw
+icxhvbxoqtunleuzpdmfksahgw
+icxjvbruqtunleyzpnmfksfhgw
+icdjvbroqtunleyzpdmfksahup
+ihxjvbroqtunleovpdmfksahgw
+icxjvbroqtunleyzxdmfksahzv
+ocxjvbioqtunleyzpdmfzsahgw
+idxjvbroqtunlyyzpdofksahgw
+izdjvbroqtunleyzpdtfksahgw
+icxjvbrnqtunleyzpdmfksbhgb
+icxjvbrjqtunleyhpdmrksahgw
+icxjvbroqtunleyzpdbflsahgg
+icxjvbmfqtunleyzpdmfkaahgw
+idxjvbroqtunlsyzpdffksahgw
+bcxjvbroqtunleyzpkmfkswhgw
+ivxjvbroqtdnleyzpdmbksahgw
+icxpvbboqtunleyzpdmfksahtw
+ibxjvbroqtunlehzpdmfkmahgw
+icxjvbboqtunleyzpdmfkaahgv
+icxjlaroqtuileyzpdmfksahgw
+icxjvbroftunleyzpdmfqsahew
+ichjvbroqtunleyzpdmiwsahgw
+icxrvbvoqtunleyzpdmiksahgw
+icxjvbroqtunldydpdmfksahgl
+icogvbroqtunleyzpdmfnsahgw
+icxjvbroqtunleszodmfkswhgw
+icxjvbrontunleyztemfksahgw
+icxjvbrovtunleyzpdvkksahgw
+icxjvbroqqucteyzpdmfksahgw
+icmovbroptunleyzpdmfksahgw
+icxjvbqoftunleyzvdmfksahgw
+icxjvbdoqtunleyzpdmfkadhgw
+icxjvbroqtunlgnzpdmfksaqgw
+icxjvbroqtunieygpdyfksahgw
+acdjvbroqtunleyzpdmfkwahgw
+icxjvbroqtunleyzpdmfkfahlj
+icxjvbgoqtunleyepdmvksahgw
+icxjvbpobbunleyzpdmfksahgw
+icxjvbroqtunleurpdmfktahgw
+ipxjvbzoqtunleyzpdmffsahgw
+icxjtbroqtunllyzpdmuksahgw
+icxjvbroqtunbsyzadmfksahgw
+ihxjvoroqtuqleyzpdmfksahgw
+idxjmbroqqunleyzpdmfksahgw
+wcxjvbdoqtunleyzpdmfksahgr
+icxjvbroqtunleygptmfksahgj
+ipxjvbrsqtunleyzpdmfksghgw
+ycxjvbroqtunluyzkdmfksahgw
+icxjvbroxtuulejzpdmfksahgw
+icqjvbroqtunlwyypdmfksahgw
+ioxjhbroqtunleyzphmfksahgw
+icxjvbgoqnunleyzpdmfksahaw
+mcxjvbroqtunleyzpdmfksihgh
+icxjsbroqtunlqyzpdmfksawgw
+icxjvbroqtuoleycpdmftsahgw
+icxjvbroqtunleyzgdifksahlw
+icxjvbmoqtunleyzjfmfksahgw
+icxjvbroqtunlezopdmfksahge
+icxjvbroqtbnlefzpdmfosahgw
+tcxjvbromtunlevzpdmfksahgw
+irxjgbroqtunleyzpdmfksthgw
+icxjvbrojtunleyxpdmoksahgw
+icxrvbroytpnleyzpdmfksahgw
+icxjvbroqtunfeyupdmfksasgw
+ihqjvbroqtunleyzpdmftsahgw
+icxjobroqkunleozpdmfksahgw
+icjjjbroqtualeyzpdmfksahgw
+icxjvbroqtunaeytpdmfksahvw
+icxjvbroqtunzeyzpdmfkshhxw
+icxqvbroqtucleyzxdmfksahgw
+icxjvbrogturleyzxdmfksahgw
+icxjvoqoqtunleyzpdcfksahgw
+iuxjvbroqtunleyzpdmfksopgw
+icxjveroqtunleyzptmfksalgw
+icxjvbroqtunleyzpdmfwcahhw
+iwxjvbroqtlnleyzpdmfksyhgw
+ectjvbroqtanleyzpdmfksahgw
+icxjvnroqtyhleyzpdmfksahgw
+icvjvhboqtunleyzpdmfksahgw
+icxjtbroqtuzleyupdmfksahgw
+icjjvproqtunleyzpsmfksahgw
+icdjvbroqtutleyzpdmiksahgw
+icxjvwroqtujleyzpdmfksahgc
+icxjxbroqtunleyzpdwhksahgw
+icxjvbqoqtunleyzpdmvfsahgw
+icajvbroqtusleyzpdmfksaagw
+icxjvbroqtunbtyzpdmfksmhgw
+kcxjvbroqtxnleyzpdmfkskhgw
+icxjvbqogfunleyzpdmfksahgw
+icxjvbroqtubleyzpdmfdswhgw
+icxjvprlqtunleyzpdmffsahgw
+icxjxbroqtucleyzpdmfksakgw
+dcxrvbroqtunleycpdmfksahgw
+icxjvbrobtunleyzpomfksahgu
+ocxrvbroqtunleyzpdmfssahgw
+icxjvbroktunlejzpdmfksahzw
+icxjvbrovtunleyzmdmfkhahgw
+icxjvbroqtudleygpdmfksfhgw
+bcxjvbroqtubllyzpdmfksahgw
+icxwvbrontunzeyzpdmfksahgw
+icxjvbroqtunleysjbmfksahgw
+icxjvvroztunleyzpdmfksjhgw
+ivxjxbroqtunleyzpdmfksahew
+icxjvbroqtunleyupqufksahgw
+icxjvmrcqtunleyzpdmxksahgw
+icxjvgroqtunleyzpdgfkuahgw
+icxjvbroqthnqeyfpdmfksahgw
+icxjsbuodtunleyzpdmfksahgw
+iuxjzbroqtunleyzpdrfksahgw
+icxjvbrobtunlelzpdmfksahgs
+icxjvbroqtzhljyzpdmfksahgw
+inxtvbroqtunleyzpdmeksahgw
+icgjvbroqtunleyztdmfksahgq
+icxjvagoqtugleyzpdmfksahgw
+icxuvbroqtunleyzpimfkyahgw
+icxzvbroqtfhleyzpdmfksahgw
+icxjjbroqtqnleyzpdmnksahgw
+icjrvbroqtunleszpdmfksahgw
+iexjvbroqtunlgyzpdmfksacgw
+rcxjvbkoqtuoleyzpdmfksahgw
+icxjvbroqgunlwyzpdmfksqhgw
+icxjvbroqtunleqzpsmfksqhgw
+icxjvbroqtubaeyzpdmfksaugw`
+
+func parseBoxIds(s string) []string {
+	return strings.Split(s, "\n")
+}
+
+func containsExactNoOfLetters(s string, noOfLettersRequired int) bool {
+	letters := map[int32]int{}
+
+	for _, l := range s {
+		letters[l] += 1
+	}
+
+	for _, number := range letters {
+		if number == noOfLettersRequired {
+			return true
+		}
+	}
+
+	return false
+}
+
+func calculateChecksum(boxIds []string) int {
+	// MEMORY EFFICIENT *HIGH FIVE*
+	counts := make(map[int]int, 2)
+	counts = map[int]int{
+		2: 0, 3: 0,
+	}
+
+	for _, b := range boxIds {
+		if containsExactNoOfLetters(b, 2) {
+			counts[2]++
+		}
+
+		if containsExactNoOfLetters(b, 3) {
+			counts[3]++
+		}
+	}
+
+	return counts[2] * counts[3]
+}
+
+func doStringsMatch(s1 string, s2 string, letterComparator func(l1 int32, l2 int32) bool) bool {
+	length := len(s1)
+
+	for i := 0; i < length; i++ {
+		if !letterComparator(rune(s1[i]), rune(s2[i])) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Closure for handling the off by one difference
+func offByOneRuneComparator() func(int32, int32) bool {
+	offByOneCount := 0
+
+	return func(l1 int32, l2 int32) bool {
+		fmt.Println(l1, "\t", l2)
+		if l1 == l2 {
+			return true
+		}
+
+		if l1 != l2 {
+			offByOneCount++
+			if offByOneCount <= 1 {
+				return true
+			}
+
+		}
+
+		return false
+	}
+}
+
+func calculateMatchingLetters(s1 string, s2 string) []string {
+	length := len(s1)
+	var matchingLetters []string
+
+	for i := 0; i < length; i++ {
+		if rune(s1[i]) == rune(s2[i]) {
+			matchingLetters = append(matchingLetters, string(s1[i]))
+		}
+	}
+
+	return matchingLetters
+}
+
+func calculateCommonBoxLetters(boxIds []string) []string {
+	var box string
+	var otherBox string
+
+	for i := 0; i < len(boxIds); i++ {
+		box = boxIds[i]
+
+		for j := 0; j < len(boxIds); j++ {
+			// don't compare the same box
+			if i == j {
+				continue
+			}
+			otherBox = boxIds[j]
+
+			if doStringsMatch(box, otherBox, offByOneRuneComparator()) {
+				return calculateMatchingLetters(box, otherBox)
+			}
+		}
+	}
+
+	fmt.Println("No match")
+	return nil
+}
+
+func main() {
+	boxIds := parseBoxIds(rawBoxIds)
+
+	fmt.Println(calculateChecksum(boxIds))
+	fmt.Println(strings.Join(calculateCommonBoxLetters(boxIds), ""))
+}
